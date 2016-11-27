d1a9a0cee3681cfe900e2bfb80c7b2e05d7d0c82
tom-cruz <tom.cruz93@gmail.com>
2016-11-27 00:29:00 -0500

    Preliminary search code with ranking by distance

---
+++ APISearch.py
@@ -0,0 +1,18 @@
+import googlemaps
+import urllib, json
+
+def GoogPlac(lat,lng,radius,types,key):
+	AUTH_KEY = key
+	LOCATION = str(lat) + "," + str(lng)
+	RADIUS = radius
+	TYPES = types
+	MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+	'?location=%s'
+	'&radius=%s'
+	'&types=%s'
+	'&sensor=false&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
+
+response = urllib.urlopen(MyUrl)
+jsonRaw = response.read()
+jsonData = json.loads(jsonRaw)
+return jsonData

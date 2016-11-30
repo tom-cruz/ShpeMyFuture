9f655fe22cca30fdc9e7f4bd53392f8557bc04f2
tom-cruz <tom.cruz93@gmail.com>
2016-11-29 22:56:13 -0500

    Trying to crete dump file

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import googleplaces
 from googleplaces import GooglePlaces, types, lang, ranking
 
-YOUR_API_KEY= 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+YOUR_API_KEY= 'AIzaSyDFu4TmJ0nqqCl3ZyRRBxe1Pkk8E8Onahs'
 
 google_places = GooglePlaces(YOUR_API_KEY)
 # You may prefer to use the text_search API, instead.
@@ -12,8 +12,10 @@ query_result = google_places.nearby_search(
 # will be send as type param to fullfil:
 # http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
 
-if query_result.has_attributions:
-    print query_result.html_attributions
+#if query_result.has_attributions:
+data = open("placesDump.txt","w")
+data.write(str(query_result))
+data.close()
 
 
 for place in query_result.places:
@@ -22,27 +24,6 @@ for place in query_result.places:
     print place.geo_location
     print place.place_id
 
-    # The following method has to make a further API call.
     place.get_details()
-    # Referencing any of the attributes below, prior to making a call to
-    #get_details() will raise a googleplaces.GooglePlacesAttributeError.
     print place.formatted_address
-    #print place.details # A dict matching the JSON response from Google.
-    #print place.local_phone_number
-    #print place.international_phone_number
-    #print place.website
-    #print place.url
 
-    # Getting place photos
-
-    #for photo in place.photos:
-        # 'maxheight' or 'maxwidth' is required
-    #    photo.get(maxheight=500, maxwidth=500)
-        # MIME-type, e.g. 'image/jpeg'
-    #    photo.mimetype
-        # Image URL
-    #    photo.url
-        # Original filename (optional)
-    #    photo.filename
-        # Raw image data
-    #    photo.data

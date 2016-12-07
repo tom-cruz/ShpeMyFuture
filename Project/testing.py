0604eba80e18660284e39c34bb4832a0068a6d7f
asperkins96 <asperkins96@gmail.com>
2016-12-07 01:59:18 -0500

    Add files via upload

---
+++ Project/testing.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import os
+import string
+import googleplaces
+from googleplaces import GooglePlaces, types, lang, ranking
+import re
+import csv
+
+f = open('cleaning.txt', 'w')
+
+YOUR_API_KEY= 'AIzaSyDFu4TmJ0nqqCl3ZyRRBxe1Pkk8E8Onahs'
+
+google_places = GooglePlaces(YOUR_API_KEY)
+# You may prefer to use the text_search API, instead.
+query_result = google_places.nearby_search(
+        location='Ewing, New Jersey', keyword='Dentist',
+        radius=500, rankby=ranking.DISTANCE, types=[types.TYPE_DENTIST,types.TYPE_ADMINISTRATIVE_AREA_LEVEL_3])
+# If types param contains only 1 item the request to Google Places API
+# will be send as type param to fullfil:
+# http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
+
+#if query_result.has_attributions:
+
+
+for place in query_result.places:
+    loc = place.geo_location
+    s = str(loc)
+    result = re.sub('[^0-9, ]', '', s)
+    f.write(result + "\n")
+
+
+
+
+"""
+f=open('cleaning.txt', 'r')
+queryOutput = f.read()
+outputParsed = re.findall(r'Decimal("'"\d+.\d'"),', queryOutput)
+with open('latlng','wb') as csvfile:
+	writer = csv.writer(csvfile)
+ 	for item in outputParsed:
+ 		writer.writerow([item])  
+ """
+    # Returned places from a query are place summaries.
+	#data = open("placesDump.txt","w")
+	#data.write(str(query_result))
+	#data.close()
+	#print place.geo_location
+	#print > f, "lkasjdflaskdjfh"
+#f.write(place.geo_location)
+	#place.geo_location
+ 
+    #for item in loc:
+	#	f.write(str(item + "\n"))
+	
+
+
+ 	

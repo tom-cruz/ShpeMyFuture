7a50ee1616a30ec57b6971bc4063de702d3d014c
asperkins96 <asperkins96@gmail.com>
2016-12-07 14:20:03 -0500

    Add files via upload

---
+++ Flaskr.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+import threading
+import subprocess
+import uuid
+
+
+
+app = Flask(__name__)
+backgroud_scripts = {}
+
+def run_script(id):
+	subpro
+
+@app.route('/')
+def main():
+	return render_template('index.html')
+@app.route('/choose/')
+def choose():
+	return render_template('choose.html')
+
+	
+@app.route('/choose/profession1/')
+def profession1():
+	return render_template('profession1.html')
+@app.route('/choose/profession2/')
+def profession2():
+	return render_template('profession2.html')
+@app.route('/choose/profession3')
+def profession3():
+	return render_template('profession3.html')
+@app.route('/choose/profession4/')
+def profession4():
+	return render_template('profession4.html')
+
+
+
+
+if __name__ == '__main__':
+	app.run()
+

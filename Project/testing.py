8d5955dd827e36456930acd3c2e80178452e3769
tom-cruz <tom.cruz93@gmail.com>
2016-12-07 05:54:19 -0500

    Created JSON file for lat and longs

--- Project/testing.py
+++ Project/testing.py
@@ -1,63 +1,56 @@
-from __future__ import print_function
-import os
-import string
 import googleplaces
 from googleplaces import GooglePlaces, types, lang, ranking
-import re
-import csv
+import re, json, itertools
 
-f = open('cleaning.txt', 'w')
-x = []
-y = []
 YOUR_API_KEY= 'AIzaSyDFu4TmJ0nqqCl3ZyRRBxe1Pkk8E8Onahs'
 
 google_places = GooglePlaces(YOUR_API_KEY)
-# You may prefer to use the text_search API, instead.
-query_result = google_places.nearby_search(
-        location='Ewing, New Jersey', keyword='Dentist',
-        radius=500, rankby=ranking.DISTANCE, types=[types.TYPE_DENTIST,types.TYPE_ADMINISTRATIVE_AREA_LEVEL_3])
-# If types param contains only 1 item the request to Google Places API
-# will be send as type param to fullfil:
-# http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
 
-#if query_result.has_attributions:
+def BizHubSearch (KEYWORD, TYPE):
 
+	f = open('cleaning.txt', 'w')
+	cities = ["North Bergen, NJ", "Lyndhurst, NJ","Paterson, NJ"]
 
-for place in query_result.places:
-    loc = place.geo_location
-    s = str(loc)
-    #result = re.sub('[^0-9, ]', '', s)
-    f.write(s+"\n")
+	for item in cities:
+		query_result = google_places.nearby_search(
+	    	    location=item, keyword=KEYWORD,
+	        	radius=500, rankby=ranking.DISTANCE, types=[TYPE,types.TYPE_ADMINISTRATIVE_AREA_LEVEL_3]) #types.TYPE_DENTIST
+		for place in query_result.places:
+			loc = place.geo_location
+	   		s = str(loc)
+	   		f.write(s+"\n")
+	return;
 
-
-
-
-
-f=open('cleaning.txt', 'r')
-queryOutput = f.read()
-outputParsed = re.split('(\d+.\d+\d|-\d+.\d+\d)', queryOutput)
-outputParsed.extend(outputParsed[1::5])
-#s = str(outputParsed)
-
-with open('latlng','wb') as csvfile:
-	writer = csv.writer(csvfile)
- 	for item in outputParsed:
- 		writer.writerow([item])
-
-
-
-    # Returned places from a query are place summaries.
-	#data = open("placesDump.txt","w")
-	#data.write(str(query_result))
-	#data.close()
-	#print place.geo_location
-	#print > f, "lkasjdflaskdjfh"
-#f.write(place.geo_location)
-	#place.geo_location
- 
-    #for item in loc:
-	#	f.write(str(item + "\n"))
+def LatLngParsing ():
 	
+	f=open('cleaning.txt', 'r')
+	queryOutput = f.read()
+	outputParsed = re.split('(\d+.\d+\d|-\d+.\d+\d)', queryOutput)
 
+	x=[]
+	x.extend(outputParsed[1::4])
+	
+	y=[]
+	y.extend(outputParsed[3::4])
+	
+	for item1,item2 in zip(x,y):
+		jsonData = json.dumps({'lat':item1,'lng':item2})
+			
+	return;
 
- 	
+"""	with open('lat.txt','wb') as csvfile:
+		writer = csv.writer(csvfile)
+	 	for item in x:
+	 		writer.writerow([item])
+
+	y=[]
+	y.extend(outputParsed[3::4])
+
+	with open('lng.txt','wb') as csvfile:	
+	 	writer = csv.writer(csvfile)
+	 	for item in y:
+	 		writer.writerow([item])
+	return;"""
+
+BizHubSearch("Doctor", types.TYPE_DOCTOR)
+LatLngParsing()
